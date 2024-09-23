df42550f8ecc242bb5beccc44d2b64860f0f3704
d6nsx <144886207+d6nsx@users.noreply.github.com>
2024-09-23 13:38:17 +0600

    Add files via upload

---
+++ project/js/home_works.js
@@ -0,0 +1,139 @@
+const gmailInput = document.querySelector('#gmail_input');
+const gmailButton = document.querySelector('#gmail_button');
+const gmailResult = document.querySelector('#gmail_result');
+
+const regExp = /^[a-z0-9]+@gmail\.com$/i
+gmailButton.onclick = () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = 'Все правильно!'
+        gmailInput.style.borderColor = 'green'
+        gmailInput.style.Color = 'green'
+    }
+    else {
+        gmailResult.innerHTML = 'Неправильно!'
+        gmailInput.style.borderColor = 'red'
+        gmailInput.style.Color = 'red'
+
+    }
+}
+
+const parentBlock = document.querySelector('.parent_block')
+const childBlock = document.querySelector('.child_block')
+let posX = 0, posY = 0
+// let maxWidth = 450
+
+
+
+const offsetWidth = parentBlock.offsetWidth - childBlock.offsetWidth
+const offsetHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+
+const moveBlock = () => {
+    if (posX <= offsetWidth && posY === 0) {
+        childBlock.style.left = `${posX}px`
+        posX++
+        // requestAnimationFrame(moveBlock)
+    } else if (posX >= offsetWidth && posY <= offsetHeight) {
+        posY++
+        childBlock.style.top = `${posY}px`
+        // requestAnimationFrame(moveBlock)
+    } else if (posY >= offsetHeight && posX >= 0) {
+        posX--
+        childBlock.style.left = `${posX}px`
+        // requestAnimationFrame(moveBlock)
+    } else if (posX <= 0 && posY >=0) {
+        console.log("d")
+        posY--
+        childBlock.style.top=`${posY}px`
+    }
+    setTimeout(moveBlock,1)
+}
+moveBlock()
+
+
+
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
+const reset = document.querySelector('#reset')
+const result = document.querySelector('#seconds')
+const resultMinute = document.querySelector('#minute')
+let click = true
+
+let seconds = 0
+let interval;
+let minute= 0
+
+
+
+const timer = () =>{
+    seconds++
+    result.innerHTML = seconds
+    if (seconds > 60){
+        minute++
+        seconds=0
+        resultMinute.innerHTML = minute
+    }
+}
+start.onclick = () => {
+    if(click === true){
+        interval= setInterval(timer, 100)
+        click = false
+    }
+
+
+}
+
+stop.onclick = () => {
+    clearInterval(interval)
+    click=true
+}
+reset.onclick = () => {
+    clearInterval(interval)
+    click=true
+    minute = 0
+    seconds = 0
+    result.innerHTML = seconds
+    resultMinute.innerHTML = minute
+
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const charactersBlock=document.querySelector('.characters_container');
+    const request=new XMLHttpRequest()
+    request.open('GET', '../data/persons.json');
+    request.setRequestHeader('Content-type','application/json');
+    request.send();
+
+    request.onload=()=>{
+        if (request.status>=200 && request.status<400) {
+            const characters=JSON.parse(request.responseText);
+            characters.map((character)=>{
+                const characterContainer=document.createElement('div');
+                characterContainer.setAttribute("class" , 'character_container');
+                characterContainer.innerHTML =`
+                <div class="character_photo">
+                    <img src="${character.photo}" alt="">
+                </div>
+                <h2>${character.name}</h2>
+                <p>${character.age}</p>
+                <p>${character.bio}</p>
+                `;
+                const h2Element=characterContainer.querySelector('h2');
+                const pElements=characterContainer.querySelectorAll('p');
+
+                if (h2Element){
+                    h2Element.style.color='black';
+                }
+                pElements.forEach(p=>{
+                    p.style.color='black';
+                });
+                charactersBlock.append(characterContainer);
+            }) ;
+        } else {
+            console.error('Request failed', request.status);
+        }
+
+    };
+    request.open.error=()=>{
+        console.error('Request failed');
+    }
+})
---
+++ project/js/lesson.js
@@ -0,0 +1,150 @@
+const phoneInput = document.querySelector('#phone_input');
+const phoneButton = document.querySelector('#phone_button');
+const phoneResult = document.querySelector('#phone_result');
+console.log(phoneButton)
+
+const regExp = /^\+996 [25793]\d{2} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.onclick = () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = 'Все правильно!'
+        phoneInput.style.borderColor = 'green'
+        phoneInput.style.Color = 'green'
+        console.log("de")
+    }
+    else {
+        phoneResult.innerHTML = 'Неправильно!'
+        phoneInput.style.borderColor = 'red'
+        phoneInput.style.Color = 'red'
+        console.log("rrr")
+    }
+
+}
+
+const tabСontentBlocks = document.querySelectorAll('.tab_content_block')
+
+const tabContentItems = document.querySelectorAll('.tab_content_item')
+const tabContentItemsParent = document.querySelector('.tab_content_items')
+
+const hideTabContent = () => {
+    tabСontentBlocks.forEach(block => {
+        block.style.display = 'none'
+    })
+    tabContentItems.forEach(item => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+let current = 0
+
+
+const showTabContent = (index=0 ) => {
+    tabСontentBlocks[index].style.display = 'block'
+    tabContentItems[index].classList.add('tab_content_item_active')
+}
+hideTabContent()
+showTabContent()
+
+tabContentItemsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabContentItems.forEach((item, index) =>{
+            if (event.target === item) {
+                hideTabContent()
+                showTabContent(index)
+                current = index
+            }
+        })
+    }
+}
+
+
+
+setInterval(()=>{
+    if(current< tabСontentBlocks.length){
+
+        hideTabContent()
+        showTabContent(current)
+        current++
+    }else{
+        current=0
+    }
+} , 5000)
+
+
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const wonInput = document.querySelector('#won');
+
+const converter = (element, targetElement1, targetElement2) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.send();
+
+        request.onload = () => {
+            const data = JSON.parse(request.response);
+
+            if (element.id === "som") {
+                targetElement1.value = (element.value / data.usd).toFixed(2);
+                targetElement2.value = (element.value * data.won).toFixed(2);
+            }
+            if (element.id === "usd") {
+                targetElement1.value = (element.value * data.usd).toFixed(2);
+                targetElement2.value = (element.value * data.won).toFixed(2);
+            }
+            if (element.id === "won") {
+                targetElement1.value = (element.value / data.won).toFixed(2);
+                targetElement2.value = (element.value / data.usd).toFixed(2);
+            }
+        };
+    };
+};
+
+converter(somInput, usdInput, wonInput);
+converter(usdInput, somInput, wonInput);
+converter(wonInput, somInput, usdInput);
+
+
+
+
+
+
+
+
+
+const card = document.querySelector('.card');
+const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector('#btn-prev');
+
+let cardId = 199;
+
+
+const updateCard = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            card.innerHTML = `
+                    <p>${data.title}</p>
+                    <p style="color: ${data.completed ? 'green' : 'red'}"
+                    </p>
+                    <span>Card ID: ${data.id}</span>
+                `;
+        });
+};
+
+
+btnNext.addEventListener('click', () => {
+    cardId = (cardId >= 200) ? 1 : cardId + 1;
+    updateCard(cardId);
+});
+
+btnPrev.addEventListener('click', () => {
+    cardId = (cardId === 1) ? 200 : cardId - 1;
+    updateCard(cardId);
+});
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => {
+        console.log(posts);
+    })
+    .catch(error => console.error('Error fetching posts:', error));
